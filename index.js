954c608ff08e87ae496e96e4d62dd9b0701af256
cmrohityadav <cmrohityadav23@gmail.com>
2025-05-17 23:45:56 +0530

    added 404 handler

--- index.js
+++ index.js
@@ -5,6 +5,15 @@ dotenv.config();
 const app=express();
 const PORT=process.env.PORT || 3000;
 
+// 404 handler
+// It should be always at bottom
+
+app.use((req,res)=>{
+    res.status(400).json({
+        status:"error",
+        message:"This route not available"
+    })
+})
 
 app.listen(PORT,()=>{
     console.log(`Server is running at ${PORT} in ${process.env.NODE_ENV} mode`)
