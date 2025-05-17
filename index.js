fa6027867f8aeff70651a8fc19f75dfa642da034
cmrohityadav <cmrohityadav23@gmail.com>
2025-05-18 00:20:25 +0530

    added global error handler and morgon logger

--- index.js
+++ index.js
@@ -1,13 +1,38 @@
 import express from 'express';
+import morgan from 'morgan';
 import dotenv from "dotenv";
 dotenv.config();
 
 const app=express();
 const PORT=process.env.PORT || 3000;
 
+//logger middleware
+if(process.env.NODE_ENV== "development"){
+    app.use(morgan('dev'));
+}
+
+// Body Parser middleware 
+app.use(express.json({limit:'10kb'}));
+app.use(express.urlencoded({extended:true,limit:'10kb'}));
+
+
+// Global Error Handler
+app.use((error,req,res,next)=>{
+    console.error(error.stack);
+    res.status(error.status || 500).json({
+        status:'error',
+        message:error.message || 'Internal server error',
+        ...(process.env.NODE_ENV=='development' && {stack:error.stack})
+    })
+})
+
+
+//API Routes
+
+
+
 // 404 handler
 // It should be always at bottom
-
 app.use((req,res)=>{
     res.status(400).json({
         status:"error",
